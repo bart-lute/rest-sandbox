b5ec6fd3eb7f96d311ade1cb169b848bf2d51e49
Bart Lute <bart.lute@gmail.com>
2013-09-13 23:43:26 +0200

    css added and application variables file created

--- src/main/java/nl/bartlute/restsb/controllers/HomeController.java
+++ src/main/java/nl/bartlute/restsb/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package nl.bartlute.restsb;
+package nl.bartlute.restsb.controllers;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
+//@RequestMapping(value = "/trala")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Value("#{applicationProps['application.version']}")
+	private String appVersion;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -32,6 +37,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("version", appVersion);
 		
 		return "home";
 	}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -3,12 +3,13 @@
 <html>
 <head>
 	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="<c:url value='/resources/css/main.css' />?${version}">
 </head>
 <body>
 <h1>
 	Hello world!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<P>Version: ${version}  The time on the server is ${serverTime}. </P>
 </body>
 </html>
